4f0c9bdce701d97346f6c2623200f7cec5994723
AlanCorn <1561206831@qq.com>
2021-07-11 14:40:48 +0800

    删除无用语句

--- src/charactor/ADAPHero.java
+++ src/charactor/ADAPHero.java
@@ -4,13 +4,11 @@ public class ADAPHero extends Hero implements AP,AD{
 
     @Override
     public void physicAttack() {
-        // TODO Auto-generated method stub
         System.out.println("进行物理攻击");
     }
 
     @Override
     public void magicAttack() {
-        // TODO Auto-generated method stub
         System.out.println("进行魔法攻击");
     }
 
